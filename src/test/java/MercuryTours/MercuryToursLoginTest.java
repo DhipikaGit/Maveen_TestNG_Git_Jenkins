63f81d56f66c71abce554728b22f9d99715b69ea
DhipikaGit <42692879+DhipikaGit@users.noreply.github.com>
2019-03-01 00:35:10 +0530

    Commit999

--- src/test/java/MercuryTours/MercuryToursLoginTest.java
+++ src/test/java/MercuryTours/MercuryToursLoginTest.java
@@ -11,7 +11,7 @@ public class MercuryToursLoginTest extends Reusable {
 
 	@Test(groups="Smoke")
 	public void login() {
-		System.out.println("MercuryTours888");
+		System.out.println("MercuryTours999");
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://newtours.demoaut.com/");
